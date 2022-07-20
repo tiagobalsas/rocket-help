c8a28a13d9fcedfc07ff81d3d321ead054c1bf24
Tiago Santos <tiago881@gmail.com>
2022-07-20 18:06:38 -0300

    feat:add types svg

--- rockethelp/src/screens/Signin.tsx
+++ rockethelp/src/screens/Signin.tsx
@@ -1,8 +1,13 @@
 import { VStack, Heading } from 'native-base';
+
+import logo from '../assets/logo_primary.svg';
+
 export function Signin() {
   return (
     <VStack flex={1} alignItems='center' bg='gray.600' px={8} pt={24}>
-      <Heading color='gray.100' fontSize='xl' mt={20} mb={6}>Acesse sua conta</Heading>
+      <Heading color='gray.100' fontSize='xl' mt={20} mb={6}>
+        Acesse sua conta
+      </Heading>
     </VStack>
   );
 }
