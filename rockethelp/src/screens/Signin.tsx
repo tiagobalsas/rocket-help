83ab3a457c5b570100f9c04d1e9a823abc7ca0e7
Tiago Santos <tiago881@gmail.com>
2022-07-20 18:09:29 -0300

    feat:add logo

--- rockethelp/src/screens/Signin.tsx
+++ rockethelp/src/screens/Signin.tsx
@@ -1,10 +1,12 @@
 import { VStack, Heading } from 'native-base';
 
-import logo from '../assets/logo_primary.svg';
+import Logo from '../assets/logo_primary.svg';
+import { Loading } from '../components/Loading';
 
 export function Signin() {
   return (
     <VStack flex={1} alignItems='center' bg='gray.600' px={8} pt={24}>
+      <Logo />
       <Heading color='gray.100' fontSize='xl' mt={20} mb={6}>
         Acesse sua conta
       </Heading>
