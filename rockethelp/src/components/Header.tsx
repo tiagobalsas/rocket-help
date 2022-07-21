53012864f35e602afddf9289b8766e27195a5313
Tiago Santos <tiago881@gmail.com>
2022-07-21 15:32:16 -0300

    feat:add handleGoBack

--- rockethelp/src/components/Header.tsx
+++ rockethelp/src/components/Header.tsx
@@ -1,3 +1,4 @@
+import { useNavigation } from '@react-navigation/native';
 import {
   Heading,
   HStack,
@@ -13,6 +14,12 @@ type Props = StyledProps & {
 
 export function Header({ title, ...rest }) {
   const { colors } = useTheme();
+
+  const navigation = useNavigation();
+
+  function handleGoBack() {
+    navigation.goBack();
+  }
   return (
     <HStack
       w='full'
@@ -23,7 +30,10 @@ export function Header({ title, ...rest }) {
       pt={12}
       {...rest}
     >
-      <IconButton icon={<CaretLeft color={colors.gray[200]} size={24} />} />
+      <IconButton
+        icon={<CaretLeft color={colors.gray[200]} size={24} />}
+        onPress={handleGoBack}
+      />
       <Heading
         color='gray.100'
         textAlign='center'
