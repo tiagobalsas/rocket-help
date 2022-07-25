6a8005d339edd77a2fb99176223c54e1810c5a4c
Tiago Santos <tiago881@gmail.com>
2022-07-25 20:03:54 -0300

    App login

--- rockethelp/src/routes/index.tsx
+++ rockethelp/src/routes/index.tsx
@@ -1,12 +1,31 @@
+import { useState, useEffect } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
+import auth, { FirebaseAuthTypes } from '@react-native-firebase/auth';
 
 import { Signin } from '../screens/Signin';
+import { Loading } from '../components/Loading';
 import { AppRoutes } from './app.routes';
 
 export function Routes() {
+  const [loading, setIsloading] = useState(true);
+  const [user, setUser] = useState<FirebaseAuthTypes.User>();
+
+  useEffect(() => {
+    const subscriber = auth().onAuthStateChanged((response) => {
+      setUser(response);
+      setIsloading(false);
+    });
+
+    return subscriber;
+  }, []);
+
+  if (loading) {
+    return <Loading />;
+  }
+
   return (
     <NavigationContainer>
-      <Signin />
+      {user ? <AppRoutes /> : <Signin />}
     </NavigationContainer>
   );
 }
