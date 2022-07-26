51eb99769e16b06ce9b71f0a0199f61b06db590f
Tiago Santos <tiago881@gmail.com>
2022-07-26 10:38:08 -0300

    feat:add card details

--- rockethelp/src/screens/Details.tsx
+++ rockethelp/src/screens/Details.tsx
@@ -2,13 +2,25 @@ import { useState, useEffect } from 'react';
 
 import firestore from '@react-native-firebase/firestore';
 
+import {
+  CircleWavyCheck,
+  Hourglass,
+  DesktopTower,
+  Clipboard,
+} from 'phosphor-react-native';
+
 import { OrderFirestoreDTO } from '../DTOs/OrderFirestoreDTO';
 import { OrderProps } from '../components/Order';
 
 import { useRoute } from '@react-navigation/native';
-import { Text, VStack } from 'native-base';
+import { HStack, ScrollView, Text, useTheme, VStack } from 'native-base';
 import { Header } from '../components/Header';
+import { Loading } from '../components/Loading';
+import { CardDetails } from '../components/CardDetails';
+
 import { dateFormat } from '../utils/firestoreDateFormat';
+import { Input } from '../components/Input';
+import { Button } from '../components/Button';
 
 type RouteParams = {
   orderId: string;
@@ -21,6 +33,8 @@ type OrderDetails = OrderProps & {
 };
 
 export function Details() {
+  const { colors } = useTheme();
+
   const [isloading, setIsloading] = useState(true);
   const [solution, setSolution] = useState('');
   const [order, setOrder] = useState<OrderDetails>({} as OrderDetails);
@@ -59,10 +73,61 @@ export function Details() {
         setIsloading(false);
       });
   }, []);
+
+  if (isloading) {
+    return <Loading />;
+  }
+
   return (
     <VStack flex={1} bg='gray.700'>
       <Header title='Solicitação' />
-      <Text color='white'>{orderId}</Text>
+      <HStack bg='gray.500' justifyContent='center' p={4}>
+        {order.status === 'closed' ? (
+          <CircleWavyCheck size={22} color={colors.green[300]} />
+        ) : (
+          <Hourglass size={22} color={colors.secondary[700]} />
+        )}
+        <Text
+          fontSize='sm'
+          color={
+            order.status === 'closed'
+              ? colors.green[300]
+              : colors.secondary[700]
+          }
+          ml={2}
+          textTransform='uppercase'
+        >
+          {order.status === 'closed' ? 'finalizado' : 'em andamento'}
+        </Text>
+      </HStack>
+
+      <ScrollView mx={4} showsVerticalScrollIndicator={false}>
+        <CardDetails
+          title='equipament'
+          description={`Patrimônio ${order.patrimony}`}
+          icon={DesktopTower}
+          footer={order.when}
+        />
+        <CardDetails
+          title='Descrição do problema'
+          description={`${order.description}`}
+          icon={Clipboard}
+        />
+        <CardDetails
+          title='Solução'
+          icon={CircleWavyCheck}
+          footer={order.closed && `Encerrado em ${order.closed}`}
+        >
+          <Input
+            placeholder='Descrição da solução'
+            onChangeText={setSolution}
+            textAlignVertical='top'
+            multiline
+            h={24}
+          />
+        </CardDetails>
+      </ScrollView>
+      {order.status === 'open' && <Button title='Encerrar solicitação' m={5} />}
     </VStack>
   );
 }
